b58310ce8d420308ec9865681c4b888095836c34
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2021-11-10 22:12:03 -0500

    tested the previous 2 commits

--- TestClasses/TestTaskClass.java
+++ TestClasses/TestTaskClass.java
@@ -61,6 +61,37 @@ public class TestTaskClass {
 		Task constructor4 = new Task(2020, 2, 2, 2, 2,"testing constructor 4", 2021, 1, 1, 1, 1);
 		System.out.println(constructor4);
 		System.out.println(constructor4);
+		/*	
+		System.out.println("test compare to method");
+		Task task1 = new Task("task 1", LocalDateTime.now());
+		Task task2 = new Task("task 2", 2019, 3, 1, 1, 1);
+		System.out.println(task1.compareTo(task2));
+		System.out.println();
 
+		System.out.println("test compare to method equal");
+		task1 = new Task("task 1", 2019, 3, 1, 1, 1);
+		task2 = new Task("task 2", 2019, 3, 1, 1, 1);
+		System.out.println(task1.compareTo(task2));
+		System.out.println();
+		
+		System.out.println("test compare to method less than");
+		task1 = new Task("task 1", LocalDateTime.now());
+		task2 = new Task("task 2", 2019, 3, 1, 1, 1);
+		System.out.println(task2.compareTo(task1));
+		System.out.println();
+		*/
+
+		System.out.println("test sorting to do list");
+		
+		ToDoList list1 = new ToDoList();
+		
+		System.out.println(list1.getTasksList());
+		list1.addTask(new Task("task1", 2019, 2, 2, 2, 2));
+		System.out.println(list1.getTasksList());	
+		list1.addTask(new Task("task2", 2021, 2, 2, 2, 2));
+		System.out.println(list1.getTasksList());
+		list1.addTask(new Task("task3", 2021, 5, 2, 2, 2));
+		System.out.println(list1.getTasksList());
+		System.out.println();
 	}
 }
