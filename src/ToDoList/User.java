61d6df02125c9529e3a26c1ca3c1e890798e24c1
AbdelhalemKassara <abdelhalemkassara@gmail.com>
2021-11-19 22:50:35 -0500

    create user class

---
+++ src/ToDoList/User.java
@@ -0,0 +1,20 @@
+package ToDoList;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class User extends ToDoList{
+
+    public User(){
+    }
+    public User(LinkedList<Task> taskList){
+        super(taskList);
+
+    }
+    public User(HashMap<String, ToDoList> subList){
+        super(subList);
+    }
+    public User(HashMap<String, ToDoList> subList, LinkedList<Task> taskList){
+            super(subList, taskList);
+    }
+}
