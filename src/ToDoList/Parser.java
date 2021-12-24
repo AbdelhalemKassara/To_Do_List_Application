43a0a44a5556e43781359c50fa2526608f9e77aa
Abdel Kassara <abdelhalemkassara@gmail.com>
2021-12-23 23:54:55 -0500

    add notes for what the parser should do

---
+++ src/ToDoList/Parser.java
@@ -0,0 +1,31 @@
+package ToDoList;
+/* store the current "directory"
+create new list in some list
+create new task in some list
+create a new table
+
+add a list to a table
+
+move a list into another list
+move a task into another list
+
+print all list names at the current location
+print list at the current location
+print all table names
+print some table
+print all the lists from the current location (including sublists)
+print all lists (from root)
+
+delete a task
+delete a table
+delete a list
+
+change the date of a task
+change the "task" of a task
+change the name of a list
+change the name of a table
+ */
+public class Parser {
+    private String dir;
+}
+
